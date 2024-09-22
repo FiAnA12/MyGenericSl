6570a4e7b45eae8d5d48e5ad22d33bd93b16278b
fiana <fiana@DESKTOP-JNCFP2N>
2024-09-22 01:33:18 -0400

    Add project files.

---
+++ MyGenericSl/MyGenericClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class MyGenericClass<T>
+{
+    private T genericField;
+
+    //value of the generic field
+    public void SetGenericField(T value)
+    {
+        genericField = value;
+    }
+
+ 
+    public T GetGenericField()
+    {
+        return genericField;
+    }
+}
+
+// display the field value
+public static class GenericMethods
+{
+    public static void DisplayFieldValue<T>(MyGenericClass<T> obj)
+    {
+        Console.WriteLine($"Field value: {obj.GetGenericField()}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //instance with an integer
+        MyGenericClass<int> intInstance = new MyGenericClass<int>();
+        intInstance.SetGenericField(10);
+        GenericMethods.DisplayFieldValue(intInstance); 
+
+        //instance with a string
+        MyGenericClass<string> stringInstance = new MyGenericClass<string>();
+        stringInstance.SetGenericField("Hi, Fadji!");
+        GenericMethods.DisplayFieldValue(stringInstance); 
+    }
+}
+
